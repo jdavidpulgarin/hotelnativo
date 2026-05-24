267e4d408a310438a1a350038a33f12f5930fe59
Jarrison Pulgarin <jdavidpulgarin@unicesar.edu.co>
2026-05-24 12:18:31 -0500

    fix: corregir sourceDirectory en pom.xml para resolver paquetes correctamente

---
+++ src/main/java/com/hotel/service/ChatbotService.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.hotel.service;
+
+import com.hotel.dao.interfaces.IClienteDAO;
+import com.hotel.dao.interfaces.IHabitacionDAO;
+import com.hotel.dao.interfaces.IReservaBusqueda;
+import com.hotel.dao.interfaces.IReservaDAO;
+import com.hotel.dto.BusquedaDisponibilidadDTO;
+import com.hotel.model.Cliente;
+import com.hotel.model.Habitacion;
+import com.hotel.model.Reserva;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+/**
+ *
+ * @author Pulgarin
+ */
+public class ChatbotService {
+    
+}
