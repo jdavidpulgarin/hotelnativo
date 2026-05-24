0b437b20b7dfe4e1b705165d69042d43208197cd
Jarrison Pulgarin <jdavidpulgarin@unicesar.edu.co>
2026-05-24 11:21:15 -0500

    feat: crear clase AuditoriaService con inyección de dependencia

---
+++ src/main/java/com/hotel/service/AuditoriaService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.hotel.service;
+
+/**
+ *
+ * @author Pulgarin
+ */
+public class AuditoriaService {
+
+    private final IAuditoriaDAO auditoriaDAO;
+
+    public AuditoriaService(IAuditoriaDAO auditoriaDAO) {
+        this.auditoriaDAO = auditoriaDAO;
+    }
+}
