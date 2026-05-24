24b958d7f0645aa3612be8a89ecf820b6e404b52
Jarrison Pulgarin <jdavidpulgarin@unicesar.edu.co>
2026-05-23 23:03:20 -0500

    feat: crear clase HabitacionDTO con atributos básicos

---
+++ src/main/java/com/hotel/dto/HabitacionDTO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.hotel.dto;
+
+/**
+ *
+ * @author Pulgarin
+ */
+public class HabitacionDTO {
+
+    private String numero;
+    private int idTipoHabitacion;
+    private int idPiso;
+    private double precioBase;
+    
+    
+}
