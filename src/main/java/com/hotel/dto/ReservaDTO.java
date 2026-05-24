da346f86840080d661fb3cceee55ac4542332b06
Jarrison Pulgarin <jdavidpulgarin@unicesar.edu.co>
2026-05-23 22:43:40 -0500

    feat: crear clase ReservaDTO con atributos básicos

---
+++ src/main/java/com/hotel/dto/ReservaDTO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.hotel.dto;
+
+import java.time.LocalDate;
+/**
+ *
+ * @author Pulgarin
+ */
+
+/**
+ * DTO para crear una nueva reserva.
+ * Transporta solo los datos necesarios desde la vista al servicio.
+ */
+public class ReservaDTO {
+    private int idCliente;
+    private int idHabitacion;
+    private LocalDate fechaEntrada;
+    private LocalDate fechaSalida;
+    private int numPersonas;
+
+}
