77f300199538dac96b6d39455afddef9a92c1bf1
Roberto Buelvas <rcarlosbuelvas@unicesar.edu.co>
2026-05-25 12:11:34 -0500

    feat: agregar excepcion ExcepcionReservaNoEncontrada para reservas no encontradass

--- src/main/java/com/hotel/exception/ExcepcionReservaNoEncontrada.java
+++ src/main/java/com/hotel/exception/ExcepcionReservaNoEncontrada.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.hotel.exception;
 
 /**
  *
  * @author rober
  */
-public class ExcepcionReservaNoEncontrada {
-    
+
+/**
+ * Excepción cuando no se encuentra una reserva por su identificador.
+ */
+public class ExcepcionReservaNoEncontrada extends ExcepcionNegocio {
+
+    public ExcepcionReservaNoEncontrada(int idReserva) {
+        super("RESERVA_NO_ENCONTRADA",
+              "No se encontró la reserva con ID: " + idReserva);
+    }
 }
