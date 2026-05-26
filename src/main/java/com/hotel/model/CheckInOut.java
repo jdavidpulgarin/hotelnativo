19482c9814cc27d0294b25769cd9d9274517cb17
Roberto Buelvas <rcarlosbuelvas@unicesar.edu.co>
2026-05-25 19:40:31 -0500

    feat: agregar clase CheckInOut con atributos y constructor basico

---
+++ src/main/java/com/hotel/model/CheckInOut.java
@@ -0,0 +1,32 @@
+
+package com.hotel.model;
+
+/**
+ *
+ * @author rober
+ */
+import java.time.LocalDateTime;
+
+/**
+ * Entidad CheckInOut del hotel.
+ */
+public class CheckInOut {
+
+    private int id;
+    private Reserva reserva;
+    private Empleado empleadoResponsable;
+    private LocalDateTime fechaHoraCheckin;
+    private LocalDateTime fechaHoraCheckout;
+    private String observaciones;
+
+    public CheckInOut() {}
+
+    public CheckInOut(int id, Reserva reserva, Empleado empleadoResponsable,
+                      LocalDateTime fechaHoraCheckin) {
+        this.id = id;
+        this.reserva = reserva;
+        this.empleadoResponsable = empleadoResponsable;
+        this.fechaHoraCheckin = fechaHoraCheckin;
+    }
+    
+}
