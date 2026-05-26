2842772e1ed600c9dae4760caf0237906b6e120d
Roberto Buelvas <rcarlosbuelvas@unicesar.edu.co>
2026-05-25 19:38:37 -0500

    feat: agregar getters y setters en Cargo

--- src/main/java/com/hotel/model/Cargo.java
+++ src/main/java/com/hotel/model/Cargo.java
@@ -20,5 +20,15 @@ public class Cargo {
         this.descripcion = descripcion;
         this.salarioBase = salarioBase;
     }
-    
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombreCargo() { return nombreCargo; }
+    public void setNombreCargo(String nombreCargo) { this.nombreCargo = nombreCargo; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public double getSalarioBase() { return salarioBase; }
+    public void setSalarioBase(double salarioBase) { this.salarioBase = salarioBase; }
 }
