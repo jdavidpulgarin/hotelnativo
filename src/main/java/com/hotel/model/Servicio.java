204cae8d731cef1562844de8a67ffa87ca4a6ffe
Roberto Buelvas <rcarlosbuelvas@unicesar.edu.co>
2026-06-04 23:06:43 -0500

    feat: agregar clase Servicio con atributos y metodos basicos

--- src/main/java/com/hotel/model/Servicio.java
+++ src/main/java/com/hotel/model/Servicio.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.hotel.model;
 
 /**
@@ -9,5 +6,37 @@ package com.hotel.model;
  * @author rober
  */
 public class Servicio {
-    
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private boolean disponible;
+
+    public Servicio() {}
+
+    public Servicio(int id, String nombre, String descripcion, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.disponible = true;
+    }
+
+    public boolean estaDisponible() { return disponible; }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public double getPrecio() { return precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
+
+    public boolean isDisponible() { return disponible; }
+    public void setDisponible(boolean disponible) { this.disponible = disponible; }
 }
