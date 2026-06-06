61d1cf9d67ba7e629391960ae5959ad0930d99cc
Jarrison Pulgarin <jdavidpulgarin@unicesar.edu.co>
2026-06-05 22:09:34 -0500

    feat: crear clase ExcepcionBaseDatos para errores de infraestructura

--- src/main/java/com/hotel/exception/ExcepcionBaseDatos.java
+++ src/main/java/com/hotel/exception/ExcepcionBaseDatos.java
@@ -8,6 +8,17 @@ package com.hotel.exception;
  *
  * @author Pulgarin
  */
-public class ExcepcionBaseDatos {
-    
+/**
+ * Excepción para errores de acceso a base de datos. Separa errores de
+ * infraestructura de errores de negocio.
+ */
+public class ExcepcionBaseDatos extends RuntimeException {
+
+    public ExcepcionBaseDatos(String mensaje) {
+        super(mensaje);
+    }
+
+    public ExcepcionBaseDatos(String mensaje, Throwable causa) {
+        super(mensaje, causa);
+    }
 }
