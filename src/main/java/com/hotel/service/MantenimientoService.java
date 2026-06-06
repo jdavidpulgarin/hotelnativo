291df5d72740891ba7e6262cfae282b00edb1725
Jarrison Pulgarin <jdavidpulgarin@unicesar.edu.co>
2026-06-05 21:16:33 -0500

    feat: implementar solicitarMantenimiento con bloqueo de habitacion

--- src/main/java/com/hotel/service/MantenimientoService.java
+++ src/main/java/com/hotel/service/MantenimientoService.java
@@ -38,36 +38,45 @@ public class MantenimientoService {
 
         Habitacion habitacion = obtenerHabitacionOLanzarError(numeroHabitacion);
         Empleado empleado = obtenerEmpleadoOLanzarError(idEmpleado);
-cat > src/main/java/com/hotel/service/MantenimientoService.java << 'JAVAEOF'
-package com.hotel.service;
 
-import com.hotel.dao.interfaces.*;
-import com.hotel.exception.ExcepcionNegocio;
-import com.hotel.exception.ExcepcionValidacion;
-import com.hotel.model.*;
-import com.hotel.util.ValidadorEntradas;
+        if (Habitacion.EstadoHabitacion.OCUPADA.equals(habitacion.getEstado())) {
+            throw new ExcepcionNegocio("HABITACION_OCUPADA",
+                    "No se puede programar mantenimiento: la habitacion "
+                    + habitacion.getNumero() + " esta actualmente OCUPADA con un huesped.");
+        }
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
+        verificarSinReservasActivasProximas(habitacion);
 
-public class MantenimientoService {
+        habitacion.enviarAMantenimiento();
+        habitacionDAO.actualizarEstado(numeroHabitacion, Habitacion.EstadoHabitacion.MANTENIMIENTO.name());
 
-    private final IMantenimientoDAO mantenimientoDAO;
-    private final IHabitacionDAO habitacionDAO;
-    private final IEmpleadoDAO empleadoDAO;
-    private final IReservaBusqueda reservaDAO;
-    private final EmailService emailService;
+        Mantenimiento solicitud = new Mantenimiento(0, habitacion, empleado, LocalDate.now(), tipo);
+        solicitud.setDescripcionTrabajo(descripcion);
+        Mantenimiento guardado = mantenimientoDAO.insertar(solicitud);
 
-    public MantenimientoService(IMantenimientoDAO mantenimientoDAO,
-            IHabitacionDAO habitacionDAO,
-            IEmpleadoDAO empleadoDAO,
-            IReservaBusqueda reservaDAO,
-            EmailService emailService) {
-        this.mantenimientoDAO = mantenimientoDAO;
-        this.habitacionDAO = habitacionDAO;
-        this.empleadoDAO = empleadoDAO;
-        this.reservaDAO = reservaDAO;
-        this.emailService = emailService;
+        emailService.notificarNuevoMantenimiento(empleado, habitacion, tipo, descripcion, guardado.getId());
+
+        return guardado;
+    }
+
+    private Habitacion obtenerHabitacionOLanzarError(String numero) throws ExcepcionNegocio {
+        return habitacionDAO.buscarPorNumero(numero)
+                .orElseThrow(() -> new ExcepcionNegocio("HABITACION_NOT_FOUND",
+                "No se encontro la habitacion numero: " + numero));
+    }
+
+    private Empleado obtenerEmpleadoOLanzarError(int id) throws ExcepcionNegocio {
+        return empleadoDAO.buscarPorId(id)
+                .orElseThrow(() -> new ExcepcionNegocio("EMPLEADO_NOT_FOUND",
+                "No se encontro el empleado con ID: " + id));
+    }
+
+    private void verificarSinReservasActivasProximas(Habitacion habitacion) throws ExcepcionNegocio {
+    }
+
+    private Mantenimiento obtenerMantenimientoOLanzarError(int id) throws ExcepcionNegocio {
+        return mantenimientoDAO.buscarPorId(id)
+                .orElseThrow(() -> new ExcepcionNegocio("MANTENIMIENTO_NOT_FOUND",
+                "No se encontro el mantenimiento con ID: " + id));
     }
 }
