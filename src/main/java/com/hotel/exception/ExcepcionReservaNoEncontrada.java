12002184fa468e30b7713bc1debd7324a55c51ca
Jarrison Pulgarin <jdavidpulgarin@unicesar.edu.co>
2026-06-05 22:23:29 -0500

    feat: crear clase ExcepcionReservaNoEncontrada en excepciones

---
+++ src/main/java/com/hotel/exception/ExcepcionReservaNoEncontrada.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hotel.exception;
+
+/**
+ *
+ * @author Pulgarin
+ */
+/**
+ * Excepción cuando no se encuentra una reserva por su identificador.
+ */
+public class ExcepcionReservaNoEncontrada {
+
+    public ExcepcionReservaNoEncontrada(int idReserva) {
+        super("RESERVA_NO_ENCONTRADA",
+                "No se encontró la reserva con ID: " + idReserva);
+    }
+}
