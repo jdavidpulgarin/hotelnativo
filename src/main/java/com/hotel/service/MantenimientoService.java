10effb2f0c443de1fcbfb672a47ee554d414ad44
Jarrison Pulgarin <jdavidpulgarin@unicesar.edu.co>
2026-06-05 21:16:32 -0500

    feat: crear clase MantenimientoService con dependencias y constructor

--- src/main/java/com/hotel/service/MantenimientoService.java
+++ src/main/java/com/hotel/service/MantenimientoService.java
@@ -29,4 +29,45 @@ public class MantenimientoService {
         this.reservaDAO = reservaDAO;
         this.emailService = emailService;
     }
+
+    public Mantenimiento solicitarMantenimiento(String numeroHabitacion, int idEmpleado,
+            Mantenimiento.TipoMantenimiento tipo,
+            String descripcion) throws ExcepcionNegocio {
+        ValidadorEntradas.validarCampoRequerido(numeroHabitacion, "habitacion");
+        ValidadorEntradas.validarIdPositivo(idEmpleado, "empleado");
+
+        Habitacion habitacion = obtenerHabitacionOLanzarError(numeroHabitacion);
+        Empleado empleado = obtenerEmpleadoOLanzarError(idEmpleado);
+cat > src/main/java/com/hotel/service/MantenimientoService.java << 'JAVAEOF'
+package com.hotel.service;
+
+import com.hotel.dao.interfaces.*;
+import com.hotel.exception.ExcepcionNegocio;
+import com.hotel.exception.ExcepcionValidacion;
+import com.hotel.model.*;
+import com.hotel.util.ValidadorEntradas;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MantenimientoService {
+
+    private final IMantenimientoDAO mantenimientoDAO;
+    private final IHabitacionDAO habitacionDAO;
+    private final IEmpleadoDAO empleadoDAO;
+    private final IReservaBusqueda reservaDAO;
+    private final EmailService emailService;
+
+    public MantenimientoService(IMantenimientoDAO mantenimientoDAO,
+            IHabitacionDAO habitacionDAO,
+            IEmpleadoDAO empleadoDAO,
+            IReservaBusqueda reservaDAO,
+            EmailService emailService) {
+        this.mantenimientoDAO = mantenimientoDAO;
+        this.habitacionDAO = habitacionDAO;
+        this.empleadoDAO = empleadoDAO;
+        this.reservaDAO = reservaDAO;
+        this.emailService = emailService;
+    }
 }
